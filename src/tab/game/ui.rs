29ce152a7ec2e46c057f9fdae2ad898dfbb62d63
Ivan1F <wyf0762@foxmail.com>
2024-05-12 07:19:57 -0600

    score text

--- src/tab/game/ui.rs
+++ src/tab/game/ui.rs
@@ -14,7 +14,10 @@ impl Plugin for GameUiPlugin {
         app.add_systems(Startup, setup_combo_ui_system)
             .add_systems(Update, update_combo_system)
             .add_systems(Update, hide_combo_below_3_system)
-            .add_systems(Update, update_text_scale_system);
+            .add_systems(Update, update_combo_text_scale_system)
+            .add_systems(Startup, spawn_score_ui_system)
+            .add_systems(Update, update_score_text_scale_system)
+            .add_systems(Update, update_score_system);
     }
 }
 
@@ -92,6 +95,39 @@ fn setup_combo_ui_system(mut commands: Commands, asset_server: Res<AssetServer>)
         });
 }
 
+#[derive(Component)]
+struct ScoreText;
+
+fn spawn_score_ui_system(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(NodeBundle {
+        style: Style {
+            width: Val::Percent(100.0),
+            justify_content: JustifyContent::FlexEnd,
+            align_items: AlignItems::FlexStart,
+            top: Val::Px(8.0),
+            right: Val::Px(8.0),
+            ..default()
+        },
+        ..default()
+    }).with_children(|parent| {
+        parent.spawn((
+            TextBundle {
+                text: Text::from_section(
+                    "0000000",
+                    TextStyle {
+                        font: asset_server.load("font/phigros.ttf"),
+                        font_size: 10.0,
+                        color: Color::WHITE,
+                        ..default()
+                    },
+                ),
+                ..default()
+            },
+            ScoreText,
+        ));
+    });
+}
+
 fn update_combo_system(
     mut text_query: Query<&mut Text, With<ComboText>>,
     note_query: Query<&Note>,
@@ -126,15 +162,53 @@ fn hide_combo_below_3_system(
     };
 }
 
-fn update_text_scale_system(
+fn update_combo_text_scale_system(
     mut combo_text_query: Query<&mut Text, (With<ComboText>, Without<ComboIndicator>)>,
     mut combo_indicator_query: Query<&mut Text, (With<ComboIndicator>, Without<ComboText>)>,
     game_viewport: Res<GameViewport>,
 ) {
-    let scale = if game_viewport.0.width() > game_viewport.0.height() * 0.75 { game_viewport.0.height() / 18.75 } else { game_viewport.0.width() / 14.0625 };
+    let scale = if game_viewport.0.width() > game_viewport.0.height() * 0.75 {
+        game_viewport.0.height() / 18.75
+    } else {
+        game_viewport.0.width() / 14.0625
+    };
 
     let mut combo_text = combo_text_query.single_mut();
     let mut combo_indicator = combo_indicator_query.single_mut();
     combo_text.sections[0].style.font_size = scale * 1.32;
     combo_indicator.sections[0].style.font_size = scale * 1.32 * 0.4;
+}
+
+fn update_score_text_scale_system(
+    mut score_text_query: Query<&mut Text, With<ScoreText>>,
+    game_viewport: Res<GameViewport>,
+) {
+    let scale = if game_viewport.0.width() > game_viewport.0.height() * 0.75 {
+        game_viewport.0.height() / 18.75
+    } else {
+        game_viewport.0.width() / 14.0625
+    };
+
+    let mut score_text = score_text_query.single_mut();
+    score_text.sections[0].style.font_size = scale * 1.32 * 0.8;
+}
+
+
+fn update_score_system(
+    mut score_text_query: Query<&mut Text, With<ScoreText>>,
+    note_query: Query<&Note>,
+    time: Res<ChartTime>,
+    bpm_list: Res<BpmList>,
+) {
+    let notes: Vec<_> = note_query.iter().collect();
+    let combo = notes
+        .iter()
+        .filter(|note| bpm_list.time_at(note.beat) <= time.0)
+        .collect::<Vec<_>>()
+        .len();
+
+    let score = 100_0000.0 * combo as f32 / notes.len() as f32;
+
+    let mut score_text = score_text_query.single_mut();
+    score_text.sections[0].value = format!("{:07}", score.round());
 }
