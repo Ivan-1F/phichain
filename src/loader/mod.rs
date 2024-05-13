07507f967ea4fe6200a6da41651468b8137dcc92
Ivan1F <wyf0762@foxmail.com>
2024-05-13 06:04:27 -0600

    phichain chart loader

--- src/loader/mod.rs
+++ src/loader/mod.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 use bevy::prelude::*;
 
 pub mod official;
+pub mod phichain;
 
 pub trait Loader {
     fn load(file: File, commands: Commands);
---
+++ src/loader/phichain.rs
@@ -0,0 +1,42 @@
+use std::fs::File;
+
+use bevy::prelude::*;
+
+use crate::{
+    chart::{event::LineEventBundle, line::LineBundle, note::NoteBundle},
+    selection::SelectedLine,
+    serialzation::{LineWrapper, PhiChainChart},
+};
+
+use super::Loader;
+
+pub struct PhiChainLoader;
+
+impl Loader for PhiChainLoader {
+    fn load(file: File, mut commands: Commands) {
+        let chart: PhiChainChart = serde_json::from_reader(file).expect("Failed to load chart");
+        commands.insert_resource(chart.bpm_list);
+
+        let mut first_line_id: Option<Entity> = None;
+        for LineWrapper(notes, events) in chart.lines {
+            let id = commands
+                .spawn(LineBundle::new())
+                .with_children(|parent| {
+                    for note in notes {
+                        parent.spawn(NoteBundle::new(note));
+                    }
+                    for event in events {
+                        parent.spawn(LineEventBundle::new(event));
+                    }
+                })
+                .id();
+
+            match first_line_id {
+                None => first_line_id = Some(id),
+                _ => {}
+            }
+        }
+
+        commands.insert_resource(SelectedLine(first_line_id.unwrap()));
+    }
+}
