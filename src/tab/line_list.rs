01065ab29e9f162b3f4b96554e335d6965d68b3c
Ivan1F <wyf0762@foxmail.com>
2024-05-17 18:49:06 +0800

    line selection

--- src/tab/line_list.rs
+++ src/tab/line_list.rs
@@ -3,17 +3,19 @@ use crate::chart::line::Line;
 use crate::chart::note::Note;
 use bevy::prelude::*;
 use egui::Ui;
+use crate::selection::SelectedLine;
 
 // use crate::translation::Translator;
 
 pub fn line_list_tab(
     In(ui): In<&mut Ui>,
-    line_query: Query<&Children, With<Line>>,
+    line_query: Query<(&Children, Entity), With<Line>>,
     note_query: Query<&Note>,
     event_query: Query<&LineEvent>,
-    // translator: Translator,
+    
+    mut selected_line: ResMut<SelectedLine>,
 ) {
-    for (index, line) in line_query.iter().enumerate() {
+    for (index, (line, entity)) in line_query.iter().enumerate() {
         let notes = line
             .iter()
             .filter(|child| note_query.get(**child).is_ok())
@@ -24,9 +26,14 @@ pub fn line_list_tab(
             .filter(|child| event_query.get(**child).is_ok())
             .collect::<Vec<_>>()
             .len();
-        ui.label(format!(
-            "Line #{}: {} notes, {} events",
-            index, notes, events
-        ));
+        if ui
+            .selectable_label(
+                selected_line.0 == entity,
+                format!("Line #{}: {} notes, {} events", index, notes, events),
+            )
+            .clicked()
+        {
+            selected_line.0 = entity;
+        }
     }
 }
