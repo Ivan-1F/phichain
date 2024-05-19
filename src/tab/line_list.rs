9d318415dfaa79530bc79e12c392fff0dd910583
Ivan1F <wyf0762@foxmail.com>
2024-05-20 03:40:33 +0800

    show line properties in line list

--- src/tab/line_list.rs
+++ src/tab/line_list.rs
@@ -1,5 +1,5 @@
 use crate::chart::event::LineEvent;
-use crate::chart::line::Line;
+use crate::chart::line::{Line, LineOpacity, LinePosition, LineRotation};
 use crate::chart::note::Note;
 use crate::selection::SelectedLine;
 use bevy::prelude::*;
@@ -7,13 +7,22 @@ use egui::Ui;
 
 pub fn line_list_tab(
     In(ui): In<&mut Ui>,
-    line_query: Query<(&Children, Entity), With<Line>>,
+    line_query: Query<
+        (
+            &Children,
+            Entity,
+            &LinePosition,
+            &LineRotation,
+            &LineOpacity,
+        ),
+        With<Line>,
+    >,
     note_query: Query<&Note>,
     event_query: Query<&LineEvent>,
 
     mut selected_line: ResMut<SelectedLine>,
 ) {
-    for (index, (line, entity)) in line_query.iter().enumerate() {
+    for (index, (line, entity, position, rotation, opacity)) in line_query.iter().enumerate() {
         let notes = line
             .iter()
             .filter(|child| note_query.get(**child).is_ok())
@@ -27,7 +36,14 @@ pub fn line_list_tab(
         if ui
             .selectable_label(
                 selected_line.0 == entity,
-                format!("Line #{}: {} notes, {} events", index, notes, events),
+                format!(
+                    "Line #{}\n\
+                     {} notes, {} events\n\
+                     Pos: ({:.2}, {:.2})\n\
+                     Rot: {:.2}\n\
+                     Opa: {:.2}",
+                    index, notes, events, position.0.x, position.0.y, rotation.0, opacity.0
+                ),
             )
             .clicked()
         {
