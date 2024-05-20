0679f532555aae42ecad678078702b39f94a6a67
Ivan1F <wyf0762@foxmail.com>
2024-05-21 00:49:31 +0800

    added level and name text to game tab

--- src/tab/game/ui.rs
+++ src/tab/game/ui.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
 
+use crate::project::Project;
 use crate::{project::project_loaded, score::GameScore};
 
 use super::GameViewport;
@@ -22,7 +23,19 @@ impl Plugin for GameUiPlugin {
                 Update,
                 update_score_text_scale_system.run_if(project_loaded()),
             )
-            .add_systems(Update, update_score_system.run_if(project_loaded()));
+            .add_systems(Update, update_score_system.run_if(project_loaded()))
+            .add_systems(Startup, spawn_name_ui_system)
+            .add_systems(
+                Update,
+                update_name_text_scale_system.run_if(project_loaded()),
+            )
+            .add_systems(Update, update_name_system.run_if(project_loaded()))
+            .add_systems(Startup, spawn_level_ui_system)
+            .add_systems(
+                Update,
+                update_level_text_scale_system.run_if(project_loaded()),
+            )
+            .add_systems(Update, update_level_system.run_if(project_loaded()));
     }
 }
 
@@ -118,9 +131,7 @@ fn spawn_score_ui_system(mut commands: Commands, asset_server: Res<AssetServer>)
     commands
         .spawn(NodeBundle {
             style: Style {
-                width: Val::Percent(100.0),
-                justify_content: JustifyContent::FlexEnd,
-                align_items: AlignItems::FlexStart,
+                position_type: PositionType::Absolute,
                 top: Val::Px(8.0),
                 right: Val::Px(8.0),
                 ..default()
@@ -141,6 +152,72 @@ fn spawn_score_ui_system(mut commands: Commands, asset_server: Res<AssetServer>)
                     ..default()
                 },
                 ScoreText,
+            ));
+        });
+}
+
+/// Marker component to represent the name text
+#[derive(Component)]
+struct NameText;
+
+fn spawn_name_ui_system(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands
+        .spawn(NodeBundle {
+            style: Style {
+                position_type: PositionType::Absolute,
+                left: Val::Px(8.0),
+                bottom: Val::Px(8.0),
+                ..default()
+            },
+            ..default()
+        })
+        .with_children(|parent| {
+            parent.spawn((
+                TextBundle {
+                    text: Text::from_section(
+                        "Name",
+                        TextStyle {
+                            font: asset_server.load("font/phigros.ttf"),
+                            font_size: 10.0,
+                            color: Color::WHITE,
+                        },
+                    ),
+                    ..default()
+                },
+                NameText,
+            ));
+        });
+}
+
+/// Marker component to represent the level text
+#[derive(Component)]
+struct LevelText;
+
+fn spawn_level_ui_system(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands
+        .spawn(NodeBundle {
+            style: Style {
+                position_type: PositionType::Absolute,
+                bottom: Val::Px(8.0),
+                right: Val::Px(8.0),
+                ..default()
+            },
+            ..default()
+        })
+        .with_children(|parent| {
+            parent.spawn((
+                TextBundle {
+                    text: Text::from_section(
+                        "Level",
+                        TextStyle {
+                            font: asset_server.load("font/phigros.ttf"),
+                            font_size: 10.0,
+                            color: Color::WHITE,
+                        },
+                    ),
+                    ..default()
+                },
+                LevelText,
             ));
         });
 }
@@ -187,4 +264,36 @@ fn update_score_system(
 ) {
     let mut score_text = score_text_query.single_mut();
     score_text.sections[0].value = score.score_text();
+}
+
+fn update_name_text_scale_system(
+    mut name_text_query: Query<&mut Text, With<NameText>>,
+    scale: Res<TextScale>,
+) {
+    let mut score_text = name_text_query.single_mut();
+    score_text.sections[0].style.font_size = scale.0 * 1.32 * 0.5;
+}
+
+fn update_name_system(
+    mut name_text_query: Query<&mut Text, With<NameText>>,
+    project: Res<Project>,
+) {
+    let mut name_text = name_text_query.single_mut();
+    name_text.sections[0].value.clone_from(&project.meta.name);
+}
+
+fn update_level_text_scale_system(
+    mut name_text_query: Query<&mut Text, With<LevelText>>,
+    scale: Res<TextScale>,
+) {
+    let mut score_text = name_text_query.single_mut();
+    score_text.sections[0].style.font_size = scale.0 * 1.32 * 0.5;
+}
+
+fn update_level_system(
+    mut name_text_query: Query<&mut Text, With<LevelText>>,
+    project: Res<Project>,
+) {
+    let mut name_text = name_text_query.single_mut();
+    name_text.sections[0].value.clone_from(&project.meta.level);
 }
