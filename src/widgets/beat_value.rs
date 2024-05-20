ab15b494c4078ec49e1c7aca5a61fd7a45db36fd
Ivan1F <wyf0762@foxmail.com>
2024-05-21 03:01:43 +0800

    added value DragValue for BeatValue

--- src/widgets/beat_value.rs
+++ src/widgets/beat_value.rs
@@ -26,40 +26,50 @@ impl<'a> BeatValue<'a> {
 impl<'a> Widget for BeatValue<'a> {
     fn ui(self, ui: &mut Ui) -> Response {
         ui.horizontal(|ui| {
+            let mut value = self.beat.value();
+
             let mut whole = self.beat.beat();
             let mut numer = self.beat.numer();
             let mut denom = self.beat.denom();
+
             ui.add(
                 egui::DragValue::new(&mut whole)
                     .clamp_range(0..=u32::MAX)
                     .speed(1),
             );
-            ui.label(":");
             ui.add(
                 egui::DragValue::new(&mut numer)
                     .clamp_range(0..=u32::MAX)
                     .speed(1),
             );
-            ui.label("/");
             ui.add(
                 egui::DragValue::new(&mut denom)
                     .clamp_range(1..=u32::MAX)
                     .speed(1),
             );
 
+            ui.add(
+                egui::DragValue::new(&mut value)
+                    .clamp_range(0.0..=f32::MAX)
+                    .custom_formatter(|x, _| format!("{:?}", Beat::from(x as f32)))
+                    .speed(0.01),
+            );
+
             if whole != self.beat.beat() || numer != self.beat.numer() || denom != self.beat.denom()
             {
                 *self.beat = clamp_to_range(
                     Beat::new(whole, Rational32::new(numer, denom)),
-                    self.clamp_range,
+                    &self.clamp_range,
                 );
+            } else if value != self.beat.value() {
+                *self.beat = clamp_to_range(value.into(), &self.clamp_range);
             }
         })
         .response
     }
 }
 
-fn clamp_to_range(x: Beat, range: RangeInclusive<Beat>) -> Beat {
+fn clamp_to_range(x: Beat, range: &RangeInclusive<Beat>) -> Beat {
     let (mut min, mut max) = (*range.start(), *range.end());
 
     if min.cmp(&max) == Ordering::Greater {
