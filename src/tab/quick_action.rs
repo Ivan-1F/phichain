3f9b7f60a044dba23641482b399ddb53493279f8
Ivan1F <wyf0762@foxmail.com>
2024-05-28 16:32:58 +0800

    added label for quick action bar

--- src/tab/quick_action.rs
+++ src/tab/quick_action.rs
@@ -7,10 +7,13 @@ pub fn quick_action_tab(
     In(ui): In<&mut Ui>,
     mut editor_settings: ResMut<Persistent<EditorSettings>>,
 ) {
-    ui.add(
-        egui::DragValue::new(&mut editor_settings.audio.playback_rate)
-            .suffix("x")
-            .clamp_range(0.01..=1.2)
-            .speed(0.01),
-    );
+    ui.horizontal(|ui| {
+        ui.label(t!("tab.settings.category.audio.playback_rate"));
+        ui.add(
+            egui::DragValue::new(&mut editor_settings.audio.playback_rate)
+                .suffix("x")
+                .clamp_range(0.01..=1.2)
+                .speed(0.01),
+        );
+    });
 }
