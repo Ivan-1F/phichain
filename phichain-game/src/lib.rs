00305cc4182d7feaaff44e39b9b917f104796b42
Ivan1F <wyf0762@foxmail.com>
2024-08-11 16:42:15 +0800

    init phichain-game

---
+++ phichain-game/src/lib.rs
@@ -0,0 +1,30 @@
+use bevy::prelude::*;
+
+#[derive(Debug, Clone, Resource)]
+pub struct GameViewport(pub Rect);
+
+#[derive(Debug, Clone, Resource)]
+pub struct ChartTime(pub f32);
+
+#[derive(Debug, Clone, Resource)]
+pub struct GameConfig {
+    note_scale: f32,
+}
+
+#[derive(SystemSet, Debug, Clone, PartialEq, Eq, Hash)]
+pub struct GameSet;
+
+pub struct GamePlugin;
+
+impl Plugin for GamePlugin {
+    fn build(&self, app: &mut App) {
+        app.configure_sets(
+            Update,
+            GameSet.run_if(
+                resource_exists::<GameConfig>
+                    .and_then(resource_exists::<ChartTime>)
+                    .and_then(resource_exists::<GameViewport>),
+            ),
+        );
+    }
+}
