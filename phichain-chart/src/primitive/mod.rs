97de4d6ffbc99c57b88b51615009b1425cb2d221
Ivan1F <wyf0762@foxmail.com>
2024-08-17 12:42:23 +0800

    updated primitive format to 1

--- phichain-chart/src/primitive/mod.rs
+++ phichain-chart/src/primitive/mod.rs
@@ -5,12 +5,23 @@ use serde::{Deserialize, Serialize};
 
 pub mod line;
 
-#[derive(Default, Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct PrimitiveChart {
     pub format: u64,
     pub offset: f32,
     pub bpm_list: BpmList,
     pub lines: Vec<Line>,
+}
+
+impl Default for PrimitiveChart {
+    fn default() -> Self {
+        Self {
+            format: 1,
+            offset: Default::default(),
+            bpm_list: Default::default(),
+            lines: Default::default(),
+        }
+    }
 }
 
 // TODO: replace `Format` trait with this
