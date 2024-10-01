d369ca371cc1367a2e20f721566eb6af0fedfc05
Ivan1F <wyf0762@foxmail.com>
2024-10-01 17:44:05 +0800

    made `hit_effect_follow_game_time` in editor settings debug-only

--- phichain-editor/src/tab/settings/game.rs
+++ phichain-editor/src/tab/settings/game.rs
@@ -44,10 +44,13 @@ impl SettingCategory for Game {
                     finished |= response.changed();
                     ui.end_row();
 
-                    ui.label(t!("tab.settings.category.game.hit_effect_follow_game_time"));
-                    let response = ui.checkbox(&mut settings.game.hit_effect_follow_game_time, "");
-                    finished |= response.changed();
-                    ui.end_row();
+                    #[cfg(debug_assertions)]
+                    {
+                        ui.label(t!("tab.settings.category.game.hit_effect_follow_game_time"));
+                        let response = ui.checkbox(&mut settings.game.hit_effect_follow_game_time, "");
+                        finished |= response.changed();
+                        ui.end_row();
+                    }
 
                     finished
                 })
