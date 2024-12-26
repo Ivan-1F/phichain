c84210800e14e4babb715b9faf80a44c0c7b2f4b
Ivan1F <wyf0762@foxmail.com>
2024-12-25 16:46:05 -0800

    added compile step `evaluate_curve_note_tracks`

--- phichain-compiler/src/lib.rs
+++ phichain-compiler/src/lib.rs
@@ -1,13 +1,14 @@
 mod steps;
 mod utils;
 
-use crate::steps::merge_children_line;
+use crate::steps::{evaluate_curve_note_tracks, merge_children_line};
 use phichain_chart::primitive::{Format, PrimitiveChart};
 use phichain_chart::serialization::PhichainChart;
 
 /// Compile a Phichain chart into a primitive chart
 pub fn compile(chart: PhichainChart) -> anyhow::Result<PrimitiveChart> {
     let chart = merge_children_line(chart);
+    let chart = evaluate_curve_note_tracks(chart);
 
     // TODO: move into_primitive implementation here and use compile() in into_primitive
     chart.into_primitive()
--- phichain-compiler/src/steps/mod.rs
+++ phichain-compiler/src/steps/mod.rs
@@ -1,3 +1,5 @@
+mod evaluate_curve_note_tracks;
 mod merge_children_line;
 
+pub use evaluate_curve_note_tracks::evaluate_curve_note_tracks;
 pub use merge_children_line::merge_children_line;
