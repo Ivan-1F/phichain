474660523d8e46e831ea77edcc549e924f8de726
Ivan1F <wyf0762@foxmail.com>
2025-01-24 21:09:06 +0800

    implemented action panel cursor & execution

--- phichain-editor/src/tab/action_panel.rs
+++ phichain-editor/src/tab/action_panel.rs
@@ -1,10 +1,10 @@
-use crate::action::{ActionRegistrationExt, ActionRegistry};
+use crate::action::{ActionRegistrationExt, ActionRegistry, RunActionEvent};
 use crate::hotkey::modifier::Modifier;
 use crate::hotkey::{Hotkey, HotkeyRegistry};
 use bevy::app::App;
 use bevy::prelude::{
-    Commands, Component, Entity, IntoSystemConfigs, KeyCode, Plugin, Query, Res, Update, Window,
-    With, World,
+    Commands, Component, Entity, EventWriter, IntoSystemConfigs, KeyCode, Plugin, Query, Res,
+    Update, Window, With, World,
 };
 use bevy::window::PrimaryWindow;
 use bevy_egui::EguiContext;
@@ -25,7 +25,10 @@ impl Plugin for ActionPanelPlugin {
 }
 
 #[derive(Clone, Debug, Default, Component)]
-pub struct ActionPanel(String);
+pub struct ActionPanel {
+    query: String,
+    cursor: Option<usize>,
+}
 
 // TODO: using exclusive system here because somehow `mut Commands` does not work with `world.run_system()`
 fn open_action_panel_system(world: &mut World) {
@@ -43,6 +46,8 @@ fn action_panel_ui_system(
     window: Query<&Window, With<PrimaryWindow>>,
     mut context: Query<&mut EguiContext>,
     mut query: Query<(Entity, &mut ActionPanel)>,
+
+    mut run: EventWriter<RunActionEvent>,
 ) {
     let Ok((entity, mut panel)) = query.get_single_mut() else {
         return;
@@ -67,29 +72,49 @@ fn action_panel_ui_system(
 
             ui.heading("Action Panel");
             ui.separator();
-            TextEdit::singleline(&mut panel.0)
+            TextEdit::singleline(&mut panel.query)
                 .desired_width(f32::INFINITY)
                 .ui(ui)
                 .request_focus();
 
             ui.separator();
 
-            for (id, action) in &actions.0 {
-                if id.to_string() == "phichain.open_action_panel" {
-                    continue;
-                }
-                ui.horizontal(|ui| {
-                    ui.label(t!(format!("action.{}", id).as_str()));
+            let entries = actions
+                .0
+                .iter()
+                .filter(|(id, _)| id.to_string() != "phichain.open_action_panel")
+                .filter(|(id, _)| {
+                    let label = t!(format!("action.{}", id).as_str()).to_string();
+                    label.contains(panel.query.as_str())
+                        || id.to_string().contains(panel.query.as_str())
+                })
+                .collect::<Vec<_>>();
 
-                    let remain = ui.available_width();
-                    ui.add_space(remain - 200.0);
+            for (index, (id, action)) in entries.iter().enumerate() {
+                let selected = panel.cursor.is_some_and(|x| x == index);
 
-                    if action.enable_hotkey {
-                        if let Some(hotkey) = hotkeys.0.get(id) {
-                            ui.label(hotkey.to_string());
-                        }
-                    }
-                });
+                egui::Frame::none()
+                    .fill(if selected {
+                        egui::Color32::from_rgba_unmultiplied(64, 94, 168, 100)
+                    } else {
+                        egui::Color32::TRANSPARENT
+                    })
+                    .show(ui, |ui| {
+                        ui.horizontal(|ui| {
+                            ui.label(t!(format!("action.{}", id).as_str()));
+
+                            let remain = ui.available_width();
+                            ui.add_space(remain - 200.0);
+
+                            if action.enable_hotkey {
+                                if let Some(hotkey) = hotkeys.0.get(*id) {
+                                    ui.label(hotkey.to_string());
+                                }
+                            }
+
+                            ui.add_space(ui.available_width());
+                        });
+                    });
             }
 
             ui.style_mut().interaction.selectable_labels = true;
@@ -97,6 +122,44 @@ fn action_panel_ui_system(
             ui.input(|x| {
                 if x.key_pressed(egui::Key::Escape) {
                     commands.entity(entity).despawn();
+                }
+            });
+
+            // Update cursor
+
+            // 1. If the cursor is `Some` and exceeds the number of entries, update it to `Some(0)` if there is at least one entry present. Otherwise, set it to `None`
+            if panel.cursor.is_some_and(|x| x >= entries.len()) {
+                if !entries.is_empty() {
+                    panel.cursor = Some(0);
+                } else {
+                    panel.cursor = None;
+                }
+            }
+
+            // 2. if cursor is None, but at least 1 entry present, update it to Some(0)
+            if panel.cursor.is_none() && !entries.is_empty() {
+                panel.cursor = Some(0);
+            }
+
+            // 3. move cursor with up and down arrows
+            ui.input(|x| {
+                if let Some(cursor) = panel.cursor {
+                    if x.key_pressed(egui::Key::ArrowUp) {
+                        panel.cursor = Some(cursor.saturating_sub(1));
+                    } else if x.key_pressed(egui::Key::ArrowDown) {
+                        panel.cursor = Some((cursor + 1).min(entries.len() - 1));
+                    }
+                }
+            });
+
+            ui.input(|x| {
+                if x.key_pressed(egui::Key::Enter) {
+                    if let Some(cursor) = panel.cursor {
+                        if let Some(action) = entries.get(cursor) {
+                            commands.entity(entity).despawn();
+                            run.send(RunActionEvent(action.0.clone()));
+                        }
+                    }
                 }
             });
         });
