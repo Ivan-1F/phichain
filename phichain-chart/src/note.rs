7934c0133d8036d6038b18df4eb861c97123a0df
Ivan1F <wyf0762@foxmail.com>
2025-02-08 20:43:55 +0800

    improved `Debug` for `Note` and `NoteKind`

--- phichain-chart/src/note.rs
+++ phichain-chart/src/note.rs
@@ -1,15 +1,26 @@
+use crate::beat::Beat;
 use serde::{Deserialize, Serialize};
 use std::cmp::Ordering;
+use std::fmt;
 
-use crate::beat::Beat;
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case")]
 pub enum NoteKind {
     Tap,
     Drag,
     Hold { hold_beat: Beat },
     Flick,
+}
+
+impl fmt::Debug for NoteKind {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            NoteKind::Tap => write!(f, "Tap"),
+            NoteKind::Drag => write!(f, "Drag"),
+            NoteKind::Hold { hold_beat } => write!(f, "Hold({:?})", hold_beat),
+            NoteKind::Flick => write!(f, "Flick"),
+        }
+    }
 }
 
 impl NoteKind {
@@ -27,7 +38,7 @@ impl NoteKind {
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Copy, PartialEq, Serialize, Deserialize)]
 #[cfg_attr(feature = "bevy", derive(bevy::prelude::Component))]
 pub struct Note {
     pub kind: NoteKind,
@@ -35,6 +46,17 @@ pub struct Note {
     pub beat: Beat,
     pub x: f32,
     pub speed: f32,
+}
+
+impl fmt::Debug for Note {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct(format!("{:?}", &self.kind).as_str())
+            .field("above", &self.above)
+            .field("beat", &self.beat)
+            .field("x", &self.x)
+            .field("speed", &self.speed)
+            .finish()
+    }
 }
 
 impl PartialOrd for Note {
