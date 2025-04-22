5ce1018f2145a0e300c43695c950c15c8447dab1
Ivan1F <wyf0762@foxmail.com>
2025-04-22 18:11:19 +0800

    removed move_note and move_event in favor of moving

--- phichain-editor/src/editing/mod.rs
+++ phichain-editor/src/editing/mod.rs
@@ -7,8 +7,7 @@ use crate::editing::curve_note_track::CurveNoteTrackPlugin;
 use crate::editing::delete_selected::DeleteSelectedPlugin;
 use crate::editing::history::EditorHistory;
 use crate::editing::line::LineEditingPlugin;
-use crate::editing::move_event::MoveEventPlugin;
-use crate::editing::move_note::MoveNotePlugin;
+use crate::editing::moving::MovingPlugin;
 use crate::hotkey::modifier::Modifier;
 use crate::hotkey::Hotkey;
 use crate::schedule::EditorSet;
@@ -23,8 +22,7 @@ pub mod curve_note_track;
 mod delete_selected;
 pub mod history;
 mod line;
-mod move_event;
-mod move_note;
+mod moving;
 pub mod pending;
 
 pub struct EditingPlugin;
@@ -36,8 +34,7 @@ impl Plugin for EditingPlugin {
             .add_plugins(DeleteSelectedPlugin)
             .add_plugins(CreateNotePlugin)
             .add_plugins(CreateEventPlugin)
-            .add_plugins(MoveNotePlugin)
-            .add_plugins(MoveEventPlugin)
+            .add_plugins(MovingPlugin)
             .add_plugins(CurveNoteTrackPlugin)
             .add_plugins(ClipboardPlugin)
             .add_plugins(LineEditingPlugin)
---
+++ phichain-editor/src/editing/moving.rs
@@ -0,0 +1,195 @@
+use crate::action::ActionRegistrationExt;
+use crate::editing::command::event::EditEvent;
+use crate::editing::command::note::EditNote;
+use crate::editing::command::{CommandSequence, EditorCommand};
+use crate::editing::DoCommandEvent;
+use crate::hotkey::Hotkey;
+use crate::selection::Selected;
+use crate::timeline::settings::TimelineSettings;
+use bevy::prelude::*;
+use num::{FromPrimitive, Rational32};
+use phichain_chart::event::LineEvent;
+use phichain_chart::note::Note;
+
+pub struct MovingPlugin;
+
+impl Plugin for MovingPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_action(
+            "phichain.move_up",
+            move_up_system,
+            Some(Hotkey::new(KeyCode::ArrowUp, vec![])),
+        )
+        .add_action(
+            "phichain.move_down",
+            move_down_system,
+            Some(Hotkey::new(KeyCode::ArrowDown, vec![])),
+        )
+        .add_action(
+            "phichain.move_left",
+            move_left_system,
+            Some(Hotkey::new(KeyCode::ArrowLeft, vec![])),
+        )
+        .add_action(
+            "phichain.move_right",
+            move_right_system,
+            Some(Hotkey::new(KeyCode::ArrowRight, vec![])),
+        );
+    }
+}
+
+fn move_up_system(
+    timeline_settings: Res<TimelineSettings>,
+    selected_notes: Query<(&Note, Entity), With<Selected>>,
+    selected_events: Query<(&LineEvent, Entity), With<Selected>>,
+    mut event_writer: EventWriter<DoCommandEvent>,
+) {
+    if let Some((start, _)) = selected_notes.iter().min_by_key(|(note, _)| note.beat) {
+        let to = timeline_settings.attach((start.beat + timeline_settings.minimum_beat()).value());
+        let delta = to - start.beat;
+        event_writer.send(DoCommandEvent(EditorCommand::CommandSequence(
+            CommandSequence(
+                selected_notes
+                    .iter()
+                    .map(|(note, entity)| {
+                        let new_note = Note {
+                            beat: note.beat + delta,
+                            ..*note
+                        };
+                        EditorCommand::EditNote(EditNote::new(entity, *note, new_note))
+                    })
+                    .collect(),
+            ),
+        )));
+    }
+
+    if let Some((start, _)) = selected_events
+        .iter()
+        .min_by_key(|(event, _)| event.start_beat)
+    {
+        let to =
+            timeline_settings.attach((start.start_beat + timeline_settings.minimum_beat()).value());
+        let delta = to - start.start_beat;
+        event_writer.send(DoCommandEvent(EditorCommand::CommandSequence(
+            CommandSequence(
+                selected_events
+                    .iter()
+                    .map(|(event, entity)| {
+                        let new_event = LineEvent {
+                            start_beat: event.start_beat + delta,
+                            end_beat: event.end_beat + delta,
+                            ..*event
+                        };
+                        EditorCommand::EditEvent(EditEvent::new(entity, *event, new_event))
+                    })
+                    .collect(),
+            ),
+        )));
+    }
+}
+
+fn move_down_system(
+    timeline_settings: Res<TimelineSettings>,
+    selected_notes: Query<(&Note, Entity), With<Selected>>,
+    selected_events: Query<(&LineEvent, Entity), With<Selected>>,
+    mut event_writer: EventWriter<DoCommandEvent>,
+) {
+    if let Some((start, _)) = selected_notes.iter().min_by_key(|(note, _)| note.beat) {
+        let to = timeline_settings.attach((start.beat - timeline_settings.minimum_beat()).value());
+        let delta = to - start.beat;
+        event_writer.send(DoCommandEvent(EditorCommand::CommandSequence(
+            CommandSequence(
+                selected_notes
+                    .iter()
+                    .map(|(note, entity)| {
+                        let new_note = Note {
+                            beat: note.beat + delta,
+                            ..*note
+                        };
+                        EditorCommand::EditNote(EditNote::new(entity, *note, new_note))
+                    })
+                    .collect(),
+            ),
+        )));
+    }
+
+    if let Some((start, _)) = selected_events
+        .iter()
+        .min_by_key(|(event, _)| event.start_beat)
+    {
+        let to =
+            timeline_settings.attach((start.start_beat - timeline_settings.minimum_beat()).value());
+        let delta = to - start.start_beat;
+        event_writer.send(DoCommandEvent(EditorCommand::CommandSequence(
+            CommandSequence(
+                selected_events
+                    .iter()
+                    .map(|(event, entity)| {
+                        let new_event = LineEvent {
+                            start_beat: event.start_beat + delta,
+                            end_beat: event.end_beat + delta,
+                            ..*event
+                        };
+                        EditorCommand::EditEvent(EditEvent::new(entity, *event, new_event))
+                    })
+                    .collect(),
+            ),
+        )));
+    }
+}
+
+fn move_left_system(
+    timeline_settings: Res<TimelineSettings>,
+    selected_notes: Query<(&Note, Entity), With<Selected>>,
+    mut event_writer: EventWriter<DoCommandEvent>,
+) {
+    if let Some((start, _)) = selected_notes
+        .iter()
+        .min_by_key(|(note, _)| Rational32::from_f32(note.x))
+    {
+        let to = timeline_settings.attach_x(start.x - timeline_settings.minimum_lane());
+        let delta = to - start.x;
+        event_writer.send(DoCommandEvent(EditorCommand::CommandSequence(
+            CommandSequence(
+                selected_notes
+                    .iter()
+                    .map(|(note, entity)| {
+                        let new_note = Note {
+                            x: note.x + delta,
+                            ..*note
+                        };
+                        EditorCommand::EditNote(EditNote::new(entity, *note, new_note))
+                    })
+                    .collect(),
+            ),
+        )));
+    }
+}
+
+fn move_right_system(
+    timeline_settings: Res<TimelineSettings>,
+    selected_notes: Query<(&Note, Entity), With<Selected>>,
+    mut event_writer: EventWriter<DoCommandEvent>,
+) {
+    if let Some((start, _)) = selected_notes
+        .iter()
+        .max_by_key(|(note, _)| Rational32::from_f32(note.x))
+    {
+        let to = timeline_settings.attach_x(start.x + timeline_settings.minimum_lane());
+        let delta = to - start.x;
+        event_writer.send(DoCommandEvent(EditorCommand::CommandSequence(
+            CommandSequence(
+                selected_notes
+                    .iter()
+                    .map(|(note, entity)| {
+                        let new_note = Note {
+                            x: note.x + delta,
+                            ..*note
+                        };
+                        EditorCommand::EditNote(EditNote::new(entity, *note, new_note))
+                    })
+                    .collect(),
+            ),
+        )));
+    }
+}
