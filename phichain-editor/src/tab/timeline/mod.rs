09a516b4a31f5b46989510aedf0ba74cc4346612
Ivan1F <wyf0762@foxmail.com>
2025-05-25 11:21:13 +0800

    made label unselectable

--- phichain-editor/src/tab/timeline/mod.rs
+++ phichain-editor/src/tab/timeline/mod.rs
@@ -37,14 +37,17 @@ pub fn timeline_tab(In(mut ui): In<Ui>, world: &mut World) {
     {
         item.timeline.ui(&mut ui, world, item.viewport);
 
+        let name = item.timeline.name(world);
+        let galley =
+            ui.painter()
+                .layout_no_wrap(name.clone(), Default::default(), Default::default());
+
         let badge_rect = egui::Rect::from_center_size(
             egui::Pos2::new(item.viewport.center().x, 150.0),
-            egui::Vec2::new(150.0, 30.0),
+            galley.size(),
         );
 
         let visuals = ui.style().visuals.widgets.inactive;
-
-        let name = item.timeline.name(world);
 
         ui.put(badge_rect, |ui: &mut Ui| {
             ui.dnd_drag_source(Id::new(&name), index, |ui| {
@@ -56,7 +59,9 @@ pub fn timeline_tab(In(mut ui): In<Ui>, world: &mut World) {
                     epaint::StrokeKind::Inside,
                 );
                 ui.add(
-                    egui::Label::new(egui::RichText::new(&name).color(Color32::WHITE)).truncate(),
+                    egui::Label::new(egui::RichText::new(&name).color(Color32::WHITE))
+                        .truncate()
+                        .selectable(false),
                 )
             })
             .response
