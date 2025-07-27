e39eb17011c3ac9626800ea8cf1cb95264f300ca
Ivan1F <wyf0762@foxmail.com>
2025-07-27 17:16:24 +0800

    added line state preview back

--- phichain-editor/src/tab/line_list.rs
+++ phichain-editor/src/tab/line_list.rs
@@ -5,7 +5,8 @@ use crate::editing::DoCommandEvent;
 use crate::selection::SelectedLine;
 use bevy::ecs::system::SystemState;
 use bevy::prelude::*;
-use egui::{Color32, Layout, RichText, Sense, Ui};
+use egui::{Color32, Layout, RichText, Sense, Stroke, StrokeKind, Ui};
+use phichain_chart::constants::{CANVAS_HEIGHT, CANVAS_WIDTH};
 use phichain_chart::event::LineEvent;
 use phichain_chart::line::{
     Line, LineOpacity, LinePosition, LineRotation, LineSpeed, LineTimestamp,
@@ -13,6 +14,8 @@ use phichain_chart::line::{
 use phichain_chart::note::Note;
 
 const LINE_STATE_COLUMN_WIDTH: f32 = 140.0;
+
+const PREVIEW_HEIGHT: f32 = 36.0;
 
 struct LineList<'w> {
     world: &'w mut World,
@@ -52,6 +55,15 @@ impl<'a> LineList<'a> {
         });
 
         ui.horizontal(|ui| {
+            ui.scope(|ui| {
+                ui.style_mut().spacing.item_spacing = egui::Vec2::ZERO;
+                let mut show =
+                    ui.data(|data| data.get_temp("show_preview".into()).unwrap_or("state"));
+                ui.selectable_value(&mut show, "state", t!("tab.line_list.view.state"));
+                ui.selectable_value(&mut show, "preview", t!("tab.line_list.view.preview"));
+                ui.data_mut(|data| data.insert_temp("show_preview".into(), show));
+            });
+
             ui.add_space(ui.available_width() - LINE_STATE_COLUMN_WIDTH);
             ui.columns_const(|[note_event, x_y, op_rot, spd]| {
                 note_event.vertical_centered(|ui| {
@@ -209,28 +221,131 @@ impl<'a> LineList<'a> {
 
                 ui.style_mut().spacing.item_spacing = egui::Vec2::ZERO;
 
-                ui.columns_const(|[note_event, x_y, op_rot, spd]| {
-                    note_event.vertical_centered(|ui| {
-                        ui.add(trunc_label!(format!("{:.2}", notes)));
-                        ui.add(trunc_label!(format!("{:.2}", events)));
+                let show = ui.data(|data| data.get_temp("show_preview".into()).unwrap_or("state"));
+
+                if show == "preview" {
+                    ui.columns(3, |columns| {
+                        let x = position.0.x / CANVAS_WIDTH + 0.5;
+                        let y = 1.0 - (position.0.y / CANVAS_HEIGHT + 0.5);
+
+                        let width = PREVIEW_HEIGHT;
+                        let height = PREVIEW_HEIGHT / 3.0 * 2.0;
+
+                        let pos = |pos: egui::Pos2, rect: egui::Rect| {
+                            egui::Pos2::new(pos.x * rect.width(), pos.y * rect.height())
+                                + rect.min.to_vec2()
+                        };
+
+                        columns[0].vertical_centered(|ui| {
+                            // Preview
+                            let (response, painter) =
+                                ui.allocate_painter(egui::Vec2::new(width, height), Sense::hover());
+                            painter.rect_stroke(
+                                response.rect,
+                                0.0,
+                                Stroke::new(1.0, Color32::WHITE),
+                                StrokeKind::Middle,
+                            );
+
+                            let half = 1.5;
+
+                            let x1 = rotation.0.cos() * half;
+                            let y1 = rotation.0.sin() * half;
+
+                            let x2 = -x1;
+                            let y2 = -y1;
+
+                            let p1 = pos(egui::Pos2::new(-x1 + x, y1 + y), response.rect);
+                            let p2 = pos(egui::Pos2::new(-x2 + x, y2 + y), response.rect);
+                            painter.line_segment(
+                                [p1, p2],
+                                Stroke::new(
+                                    1.0,
+                                    Color32::from_rgba_unmultiplied(
+                                        255,
+                                        255,
+                                        255,
+                                        (opacity.0 * 255.0).round() as u8,
+                                    ),
+                                ),
+                            );
+                        });
+
+                        columns[1].vertical_centered(|ui| {
+                            // Position
+                            let (response, painter) =
+                                ui.allocate_painter(egui::Vec2::new(width, height), Sense::hover());
+                            painter.rect_stroke(
+                                response.rect,
+                                0.0,
+                                Stroke::new(1.0, Color32::WHITE),
+                                StrokeKind::Middle,
+                            );
+
+                            painter.circle_filled(
+                                pos(egui::Pos2::new(x, y), response.rect),
+                                2.0,
+                                Color32::WHITE,
+                            );
+                        });
+
+                        columns[2].vertical_centered(|ui| {
+                            // Opacity
+                            let (response, painter) =
+                                ui.allocate_painter(egui::Vec2::new(width, height), Sense::hover());
+                            painter.rect_stroke(
+                                response.rect,
+                                0.0,
+                                Stroke::new(1.0, Color32::WHITE),
+                                StrokeKind::Middle,
+                            );
+                            painter.rect_filled(
+                                egui::Rect::from_min_max(
+                                    response.rect.left_top(),
+                                    response.rect.center_bottom(),
+                                ),
+                                0.0,
+                                Color32::from_rgba_unmultiplied(
+                                    255,
+                                    255,
+                                    255,
+                                    (opacity.0 * 255.0).round() as u8,
+                                ),
+                            );
+                            painter.rect_filled(
+                                egui::Rect::from_min_max(
+                                    response.rect.center_top(),
+                                    response.rect.right_bottom(),
+                                ),
+                                0.0,
+                                Color32::WHITE,
+                            );
+                        });
                     });
-                    x_y.vertical_centered(|ui| {
-                        ui.add(trunc_label!(format!("{:.2}", position.0.x)));
-                        ui.add(trunc_label!(format!("{:.2}", position.0.y)));
+                } else {
+                    ui.columns_const(|[note_event, x_y, op_rot, spd]| {
+                        note_event.vertical_centered(|ui| {
+                            ui.add(trunc_label!(format!("{:.2}", notes)));
+                            ui.add(trunc_label!(format!("{:.2}", events)));
+                        });
+                        x_y.vertical_centered(|ui| {
+                            ui.add(trunc_label!(format!("{:.2}", position.0.x)));
+                            ui.add(trunc_label!(format!("{:.2}", position.0.y)));
+                        });
+                        op_rot.vertical_centered(|ui| {
+                            ui.add(trunc_label!(format!("{:.2}", opacity.0)));
+                            ui.add(trunc_label!(format!("{:.2}", rotation.0)));
+                        });
+                        spd.vertical_centered(|ui| {
+                            ui.add(trunc_label!(format!("{:.2}", speed.0)));
+                            ui.label(
+                                RichText::new(format!("#{}", self.index))
+                                    .small()
+                                    .color(Color32::WHITE),
+                            );
+                        });
                     });
-                    op_rot.vertical_centered(|ui| {
-                        ui.add(trunc_label!(format!("{:.2}", opacity.0)));
-                        ui.add(trunc_label!(format!("{:.2}", rotation.0)));
-                    });
-                    spd.vertical_centered(|ui| {
-                        ui.add(trunc_label!(format!("{:.2}", speed.0)));
-                        ui.label(
-                            RichText::new(format!("#{}", self.index))
-                                .small()
-                                .color(Color32::WHITE),
-                        );
-                    });
-                });
+                }
 
                 ui.style_mut().spacing.item_spacing = prev_item_spacing;
             });
