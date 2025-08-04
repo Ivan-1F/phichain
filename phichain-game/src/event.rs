40fedcdf32858fc8cb4f31453cfbf20947aad778
Ivan1F <wyf0762@foxmail.com>
2025-08-04 14:59:17 +0800

    derive Debug for EventOf and Events

--- phichain-game/src/event.rs
+++ phichain-game/src/event.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::{Component, Deref, Entity};
 
-#[derive(Component)]
+#[derive(Component, Debug)]
 #[relationship(relationship_target = Events)]
 pub struct EventOf(pub Entity);
 
@@ -12,6 +12,6 @@ impl EventOf {
     }
 }
 
-#[derive(Component, Deref)]
+#[derive(Component, Deref, Debug)]
 #[relationship_target(relationship = EventOf, linked_spawn)]
 pub struct Events(Vec<Entity>);
