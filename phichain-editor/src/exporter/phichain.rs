3e5b749dc22798307fb2d9b5bbfa318409aae754
Ivan1F <wyf0762@foxmail.com>
2025-08-13 16:15:53 +0800

    sort lines by `LineTimestamp` when exporting chart, fixed #31

--- phichain-editor/src/exporter/phichain.rs
+++ phichain-editor/src/exporter/phichain.rs
@@ -1,7 +1,7 @@
 use anyhow::Context;
 use bevy::prelude::*;
 use phichain_chart::bpm_list::BpmList;
-use phichain_chart::line::Line;
+use phichain_chart::line::{Line, LineTimestamp};
 use phichain_chart::offset::Offset;
 
 use super::Exporter;
@@ -16,11 +16,15 @@ impl Exporter for PhichainExporter {
         let offset = world.resource::<Offset>().0;
         let mut chart = PhichainChart::new(offset, bpm_list, vec![]);
 
-        let mut line_query = world.query_filtered::<Entity, (With<Line>, Without<ChildOf>)>();
+        let mut line_query =
+            world.query_filtered::<(Entity, &LineTimestamp), (With<Line>, Without<ChildOf>)>();
 
-        let mut lines = line_query.iter(world).collect::<Vec<_>>();
-        lines.sort();
-        for entity in lines {
+        let lines = line_query
+            .iter(world)
+            .sort::<&LineTimestamp>()
+            .collect::<Vec<_>>();
+
+        for (entity, _) in lines {
             chart
                 .lines
                 .push(SerializedLine::serialize_line(world, entity));
