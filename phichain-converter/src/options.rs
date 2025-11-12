c34127781a348196d4e92453543e709143690916
Ivan1F <wyf0762@foxmail.com>
2025-11-12 21:41:30 +0800

    use o2o for object mapping

--- phichain-converter/src/options.rs
+++ phichain-converter/src/options.rs
@@ -1,3 +1,4 @@
+use o2o::o2o;
 use phichain_chart::beat::Beat;
 use phichain_format::official::from_phichain::OfficialOutputOptions;
 use phichain_format::official::OfficialInputOptions;
@@ -5,9 +6,11 @@ use phichain_format::rpe::schema::RpeInputOptions;
 use rust_i18n::t;
 
 /// CLI wrapper for OfficialInputOptions
-#[derive(Debug, Clone, clap::Args)]
+#[derive(Debug, Clone, clap::Args, o2o)]
+#[map(OfficialInputOptions)]
 pub struct CliOfficialInputOptions {
     #[arg(long, default_value_t = false, help = t!("cli.official_input.no_easing_fitting").to_string())]
+    #[map(easing_fitting, !~)]
     no_easing_fitting: bool,
 
     #[arg(long, default_value_t = 0.1, help = t!("cli.official_input.easing_fitting_epsilon").to_string())]
@@ -17,45 +20,20 @@ pub struct CliOfficialInputOptions {
     constant_event_shrink_to: Beat,
 }
 
-impl From<CliOfficialInputOptions> for OfficialInputOptions {
-    fn from(cli: CliOfficialInputOptions) -> Self {
-        OfficialInputOptions {
-            easing_fitting: !cli.no_easing_fitting,
-            easing_fitting_epsilon: cli.easing_fitting_epsilon,
-            constant_event_shrink_to: cli.constant_event_shrink_to,
-        }
-    }
-}
-
 /// CLI wrapper for OfficialInputOptions
-#[derive(Debug, Clone, clap::Args)]
+#[derive(Debug, Clone, clap::Args, o2o)]
+#[map(OfficialOutputOptions)]
 pub struct CliOfficialOutputOptions {
     #[arg(long, value_parser = clap::value_parser!(Beat), default_value = "1/32", help = t!("cli.official_output.minimum_beat").to_string())]
     minimum_beat: Beat,
 }
 
-impl From<CliOfficialOutputOptions> for OfficialOutputOptions {
-    fn from(cli: CliOfficialOutputOptions) -> Self {
-        OfficialOutputOptions {
-            minimum_beat: cli.minimum_beat,
-        }
-    }
-}
-
-/// CLI wrapper for OfficialInputOptions
-#[derive(Debug, Clone, clap::Args)]
+// CLI wrapper for RpeInputOptions
+#[derive(Debug, Clone, clap::Args, o2o)]
+#[map(RpeInputOptions)]
 pub struct CliRpeInputOptions {
     #[arg(long, help = t!("cli.rpe_input.remove_fake_notes").to_string())]
-    pub remove_fake_notes: bool,
+    remove_fake_notes: bool,
     #[arg(long, help = t!("cli.rpe_input.remove_ui_controls").to_string())]
-    pub remove_ui_controls: bool,
-}
-
-impl From<CliRpeInputOptions> for RpeInputOptions {
-    fn from(cli: CliRpeInputOptions) -> Self {
-        RpeInputOptions {
-            remove_fake_notes: cli.remove_fake_notes,
-            remove_ui_controls: cli.remove_ui_controls,
-        }
-    }
+    remove_ui_controls: bool,
 }
